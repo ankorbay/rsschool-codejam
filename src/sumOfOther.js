acb9b85cdf0879d5239cbbf3845a4d8c8ddff28a
Anton M Korshunov <anton.m.korshunov@gmail.com>
2018-10-28 19:10:51 +0300

    style: change style according to ESlint

--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -1,12 +1,12 @@
-function sumOfOther(array){
-    const newArr = [];
-    let sumOfAll = 0;
-    const len = array.length;
-    for (let i = 0; i < len; i++){
-        sumOfAll+=array[i];  
-    }
-    for (let i = 0; i < len; i++){
-        newArr[i]=sumOfAll-array[i];
-    }
-    return newArr;
+function sumOfOther(array) {
+  const newArr = [];
+  let sumOfAll = 0;
+  const len = array.length;
+  for (let i = 0; i < len; i += 1) {
+    sumOfAll += array[i];
+  }
+  for (let i = 0; i < len; i += 1) {
+    newArr[i] = sumOfAll - array[i];
+  }
+  return newArr;
 }
